922ec56a1b812c07bff6b6f9128bc92746c01e31
ZèD <imzihad@gmail.com>
2021-11-30 19:08:14 +0600

    Imported JotForm as contact method
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,7 +1,16 @@
+// @ts-ignore
+import JotFormReact from 'jotform-react';
+
 const Contact = () => {
     return (
         <div className="">
             <h1 className="text-4xl md:text-5xl font-bold leading-normal">Contact Me</h1>
+            <JotFormReact
+                className="mt-5"
+                formURL="https://form.jotform.com/213321911862450"
+                autoResize={true}
+                autoFocus={true}
+            />
         </div>
     );
 };
