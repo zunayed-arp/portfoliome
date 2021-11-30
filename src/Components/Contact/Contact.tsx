f26011af89de2a1c3796363ff966a23f78c9add5
ZèD <imzihad@gmail.com>
2021-11-30 19:16:42 +0600

    Place a waiting text until form loaded
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,11 +1,19 @@
 // @ts-ignore
 import JotFormReact from 'jotform-react';
+import { useState } from 'react';
 
 const Contact = () => {
+    const [ loading, setLoading ] = useState<boolean>(true)
     return (
         <div className="">
             <h1 className="text-4xl md:text-5xl font-bold leading-normal">Contact Me</h1>
+            {
+                loading && <h3 className="text-2xl text-center mt-10 animate-pulse ">
+                    Please wait a moment..
+                </h3>
+            }
             <JotFormReact
+                onLoad={() => setLoading(false)}
                 className="mt-5"
                 formURL="https://form.jotform.com/213321911862450"
                 autoResize={true}
