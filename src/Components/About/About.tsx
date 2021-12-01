ec82c0ef40eff358a81cb89dd2b6b4146bb4b567
zunayed-arp <zunayed.arp@gmail.com>
2021-12-01 18:15:29 +0600

    first commit

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -3,7 +3,7 @@ const About = () => {
         <div data-aos="fade-up">
             <h1 className="text-4xl md:text-5xl font-bold leading-normal">About</h1>
             <div className="text-xl md:text-2xl mt-5 md:mt-10 space-y-5 font-semibold leading-relaxed md:leading-loose">
-                <p>My name is Md. Mofajjal Rasul Jehad. Also known as ZèD in public communities. </p>
+                <p>My name is Zunayed Patwary. </p>
                 <p> I am a full-stack web developer waiting for an opportunity to demonstrate my skills. Currently, I am working with MERN (MongoDB, ExpressJS, ReactJS, Node.JS) technologies and am pretty much comfortable working with them. I have done various projects and still working on more. </p>
                 <p> I am available for hiring at the moment.</p>
             </div>
